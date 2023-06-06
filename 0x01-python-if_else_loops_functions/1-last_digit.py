4dd61deabe6370591ffa4f32a8cbdc2ed918358d
Emad Anwer <emadanwer.official@gmail.com>
2023-06-06 14:05:46 +0200

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_diget = number  - (10 * int(number / 10))
+
+if last_diget > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, last_diget))
+elif last_diget == 0 :
+    print("Last digit of {:d} is 0 and is 0".format(number))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, last_diget))
