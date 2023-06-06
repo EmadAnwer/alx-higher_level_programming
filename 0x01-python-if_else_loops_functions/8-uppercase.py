1b51d6316eb3d540f085f865a8d79c4875d18a90
Emad Anwer <emadanwer.official@gmail.com>
2023-06-06 17:14:15 +0200

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+            print("{:c}".format(65 + (ord(c) - 97) 
+                                if ord(c) >= 97 and ord(c) <= 122 
+                                else ord(c)), end="")
+    print("")
