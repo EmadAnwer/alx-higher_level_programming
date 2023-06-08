6022de2f207d5394067fa85f3f8adade4a8ed8ba
Emad Anwer <emadanwer.official@gmail.com>
2023-06-08 15:10:47 +0200

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     count = len(argv) - 1
-    print("{:d} arguments{:s}".format(count, '.' if count == 0 else ':'))
+    print("{:d} {:s}{:s}"
+          .format
+          (
+            count,
+            'argument' if count == 1 else 'arguments',
+            '.' if count == 0 else ':'
+            ))
     for i in range(count):
         print("{:d}: {:s}".format(i + 1, argv[i + 1]))
