4d5c78921904df2c239126f3e2fac55018f928bf
Emad Anwer <emadanwer.official@gmail.com>
2023-06-08 21:56:28 +0200

    adv 1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, div, mul, sub
+    from sys import argv
+    count = len(argv) - 1
+    if count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    operators = ['+', '-', '*', '/']
+    functions = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+    for i in range(4):
+        if argv[2] == operators[i]:
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, functions[i]))
+            exit(0)
+
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
