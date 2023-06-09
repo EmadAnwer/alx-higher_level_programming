9fdd478bcbf75f96758e7153ba5144c93b717f68
Emad Anwer <emadanwer.official@gmail.com>
2023-06-09 02:26:48 +0200

    adv 3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import magic_calculation_102
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
 
     if a < b:
         c = add(a, b)
