e246f504a40de777a1e8531a71f1f6423ffca481
Emad Anwer <emadanwer.official@gmail.com>
2023-06-09 02:25:48 +0200

    adv 3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import magic_calculation_102
+def magic_calculation(a, b):
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
