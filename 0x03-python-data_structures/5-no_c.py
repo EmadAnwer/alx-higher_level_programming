4e2c1a0a851780a37486796daf0bbd92a53f4fdb
Emad Anwer <emadanwer.official@gmail.com>
2023-06-10 16:10:05 +0200

    task 5 ""

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return ""
+    if my_string is "":
+        return my_string
     new_string = ""
     for c in my_string:
         if (c is not 'c') and (c is not 'C'):
