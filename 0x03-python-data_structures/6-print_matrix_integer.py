f640510e9340e3b26fbce31d82d7a65aad35fe24
Emad Anwer <emadanwer.official@gmail.com>
2023-06-10 18:19:22 +0200

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for l in matrix:
+        list_l = len(l)
+        for i in range(list_l):
+            print("{:d}".format(l[i]), end=" " if (i != list_l - 1) else "")
+        print()
