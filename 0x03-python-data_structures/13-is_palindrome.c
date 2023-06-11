163f57932b896d20bac7b8054b1de31cf717cbd6
Emad Anwer <emadanwer.official@gmail.com>
2023-06-11 16:15:27 +0200

    is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
-
+/**
+ * is_palindrome - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: address of the new element or NULL if it fails
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
 	int len, *array_int, i;
-	
-	current = *head;
-	if(!head || !*head || !current->next) 
-		return 1;
 
-	
-    while (current != NULL) {
-        len++;
-        current = current->next;
-    }
-	
-	array_int = (int*)malloc(len * sizeof(int));
+	current = *head;
+	if (!head || !*head || !current->next)
+		return (1);
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	array_int = (int *)malloc(len * sizeof(int));
 	for (i = 0; i < len; i++)
 	{
-        array_int[i] = current->n;
-        current = current->next;
-    }
-
-	for (i = 0; i < len/2; i++)
+		array_int[i] = current->n;
+		current = current->next;
+	}
+	for (i = 0; i < len / 2; i++)
 	{
-        if (array_int[i] != array_int[len - 1 - i])
+		if (array_int[i] != array_int[len - 1 - i])
 			return (0);
-    }
-	
+	}
+
 	return (1);
 }
