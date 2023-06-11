6c0fa38e5bd91f480c974ad20166698e3f537baf
Emad Anwer <emadanwer.official@gmail.com>
2023-06-11 16:11:32 +0200

    test

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+	int len, *array_int, i;
+	
+	current = *head;
+	if(!head || !*head || !current->next) 
+		return 1;
+
+	
+    while (current != NULL) {
+        len++;
+        current = current->next;
+    }
+	
+	array_int = (int*)malloc(len * sizeof(int));
+	for (i = 0; i < len; i++)
+	{
+        array_int[i] = current->n;
+        current = current->next;
+    }
+
+	for (i = 0; i < len/2; i++)
+	{
+        if (array_int[i] != array_int[len - 1 - i])
+			return (0);
+    }
+	
+	return (1);
+}
