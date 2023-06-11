9084ddc7b439de1484cf774856c8069ec6288ae2
Emad Anwer <emadanwer.official@gmail.com>
2023-06-11 16:26:21 +0200

    tt

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,16 +10,19 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int len, *array_int, i;
-
+	int *array_int, i;
+	int len = 0;
+	
 	current = *head;
 	if (!head || !*head || !current->next)
 		return (1);
+
 	while (current != NULL)
 	{
 		len++;
 		current = current->next;
 	}
+	
 	array_int = (int *)malloc(len * sizeof(int));
 	for (i = 0; i < len; i++)
 	{
@@ -28,6 +31,7 @@ int is_palindrome(listint_t **head)
 	}
 	for (i = 0; i < len / 2; i++)
 	{
+		
 		if (array_int[i] != array_int[len - 1 - i])
 			return (0);
 	}
