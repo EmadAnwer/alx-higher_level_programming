000b225e7f23d77ddfafab9f71e6d27f34b941d2
Emad Anwer <emadanwer.official@gmail.com>
2023-06-15 00:21:24 +0200

    ls

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,44 +1,51 @@
 #include <Python.h>
 #include <stdio.h>
+/**
+ * print_python_list - adds a new node at the end of a listint_t list
+ * @p: pointer PyObject
+ */
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, i;
-    PyObject *item;
+	Py_ssize_t size, i;
+	PyObject *item;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < PyList_Size(p); i++)
-    {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
+	for (i = 0; i < PyList_Size(p); i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
-
+/**
+ * print_python_bytes - adds a new node at the end of a listint_t list
+ * @p: pointer PyObject
+ */
 void print_python_bytes(PyObject *p)
 {
-    Py_ssize_t size, i;
-    char *bytes_str;
+	Py_ssize_t size, i;
+	char *bytes_str;
 
-    printf("[.] bytes object info\n");
+	printf("[.] bytes object info\n");
 
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-    size = PyBytes_Size(p);
-    bytes_str = PyBytes_AsString(p);
+	size = PyBytes_Size(p);
+	bytes_str = PyBytes_AsString(p);
 
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", bytes_str);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", bytes_str);
 
-    printf("  first %ld bytes:", size + 1);
-    for (i = 0; i < size + 1 && i < 10; i++)
-    {
-        printf(" %.2x", bytes_str[i] & 0xFF);
-    }
-    printf("\n");
+	printf("  first %ld bytes:", size + 1);
+	for (i = 0; i < size + 1 && i < 10; i++)
+	{
+		printf(" %.2x", bytes_str[i] & 0xFF);
+	}
+	printf("\n");
 }
