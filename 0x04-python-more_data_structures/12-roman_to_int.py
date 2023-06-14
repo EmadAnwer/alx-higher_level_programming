22ea62adc56233fa501da1d206a191fb7992d858
Emad Anwer <emadanwer.official@gmail.com>
2023-06-14 22:21:07 +0200

    tas 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str or roman_string == None:
+        return 0
     roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
     r_list = []
     rez = 0
