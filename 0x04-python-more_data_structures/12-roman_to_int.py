e21a6a5966d816328f4c85e8382b12ec620d1210
Emad Anwer <emadanwer.official@gmail.com>
2023-06-14 22:17:22 +0200

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    r_list = []
+    rez = 0
+    for c in roman_string:
+        r_list.append(roman_n.get(c))
+
+    list_len = len(r_list)
+    for i in range(list_len):
+        if i == list_len - 1:
+            rez += r_list[i]
+        elif r_list[i] < r_list[i+1]:
+            rez -= r_list[i]
+        else:
+            rez += r_list[i]
+    return rez
