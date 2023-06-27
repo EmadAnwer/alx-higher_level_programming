2d68e7603112ef31118650cd5341705e04bef66c
Emad Anwer <emadanwer.official@gmail.com>
2023-06-27 18:34:16 +0200

    task 3 - 6

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """init
+        Args:
+            size: size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """area of a square"""
+        return self.__size * self.__size
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """init
+        Args:
+            size: size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area of a square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print a square of #"""
+        for i in range(self.size):
+            print("#" * self.size)
+        if self.size == 0:
+            print()
