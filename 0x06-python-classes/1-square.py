4b4ecfcc7928f158fbdf91ca9945de54f302239c
Emad Anwer <emadanwer.official@gmail.com>
2023-06-27 18:29:46 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size):
+        """init
+        Args:
+            size: size of square
+        """
+        self.__size = size
