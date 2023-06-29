f5810c92ec72f354b9b692abbb82af2e06b451a3
Emad Anwer <emadanwer.official@gmail.com>
2023-06-29 19:14:05 +0200

    t

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,5 +12,5 @@ class MagicClass:
     def area(self):
         return self.__radius ** 2 * math.pi
 
-    def circumference(self):
+    def     circumference(self):
         return 2 * math.pi * self.__radius
