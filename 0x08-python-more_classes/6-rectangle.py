253b2241173431ac2099d717d68c95d0cc657dca
Emad Anwer <emadanwer.official@gmail.com>
2023-07-03 15:03:47 +0200

    task 6 fix pycodestlye

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """Rectangle class"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """init
         Args:
@@ -15,6 +16,7 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         """width getter method"""
