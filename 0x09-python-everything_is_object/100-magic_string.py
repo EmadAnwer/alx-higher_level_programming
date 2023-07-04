164ba6fd63d889c0af481b47458185d50649c937
Emad Anwer <emadanwer.official@gmail.com>
2023-07-04 17:17:31 +0200

    adv 1

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count = magic_string.count + 1 if hasattr(magic_string, 'count') else 1
+    magic_string.count = getattr(magic_string, 'num', 0) + 1
     return magic_string.count * "BestSchool"
