c7af3849ee3a975b08edfb441482c15824347f3e
Emad Anwer <emadanwer.official@gmail.com>
2023-07-08 18:32:58 +0200

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,5 +12,6 @@ def say_my_name(first_name="", last_name=""):
         raise TypeError("first_name must be a string")
     if (not isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-
-    print(f"My name is {first_name} {last_name}")
+    if (first_name == "") and (last_name == ""):
+        raise ValueError("missing two arguments")
+    print("My name is {:s} {:s}".format(first_name, last_name))
