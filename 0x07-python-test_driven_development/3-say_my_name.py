07b373490d704d1ab77ac77a60332f3c515861e5
Emad Anwer <emadanwer.official@gmail.com>
2023-07-08 18:24:26 +0200

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,7 @@
 """say my name module"""
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """print (my_name = first_name + last_name)
     Arges:
         first_name: string
