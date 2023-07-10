850ca75667476b96bab1b2c9e4d94af2d433286f
Emad Anwer <emadanwer.official@gmail.com>
2023-07-10 20:18:24 +0200

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,7 @@
 
 class MyList(list):
     """A custom list class that inherits from the built-in list class."""
+
     def print_sorted(self):
         """
         Print the list elements in sorted order.
@@ -16,8 +17,4 @@ class MyList(list):
         Returns:
             None
         """
-        if self == []:
-            return
-        list_new = sorted(self)
-        if list_new:
-            print(list_new)
+        print(sorted(list(self)))
