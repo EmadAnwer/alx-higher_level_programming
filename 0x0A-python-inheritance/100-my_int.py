962e10e28f01a31e635d858af550a33bee21bc5a
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 01:44:14 +0200

    adv 1

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyInt module"""
+
+class MyInt(int):
+    """ MyInt class"""
+    def __eq__(self, other):
+        return int.__ne__(self, other)
+    def __ne__(self, other):
+        return int.__eq__(self, other)
