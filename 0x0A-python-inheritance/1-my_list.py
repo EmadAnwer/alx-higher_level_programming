5853797b912cf87731881ca5a20d2f9fb9a364e3
Emad Anwer <emadanwer.official@gmail.com>
2023-07-10 18:49:32 +0200

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""available attributes"""
+
+
+class MyList(list):
+    """A custom list class that inherits from the built-in list class."""
+    def print_sorted(self):
+        """
+        Print the list elements in sorted order.
+
+        Prints the list in ascending order using the sorted() function.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        print(sorted(self))
