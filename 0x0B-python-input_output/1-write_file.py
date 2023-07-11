d5d34fe4ca4db48cc5d4b3c55e9d5bce1bf9dfeb
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 12:44:58 +0200

    task 1 fix pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """write file module"""
 
+
 def write_file(filename="", text=""):
     """write_file
     args:
         filename: string
         text: string
     """
-    with open(filename, encoding="utf-8", mode= "w") as file:
+    with open(filename, encoding="utf-8", mode="w") as file:
         return file.write(text)
