d52e2c1702e2f54f1ce52752c57bc50f2b6caff0
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 21:12:37 +0200

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init
+        args:
+            first_name : string
+            last_name : string
+            age : int
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """class to json"""
+        attr_dict = self.__dict__
+        if attrs is None:
+            return attr_dict
+
+        new_dict = {}
+        for key in attrs:
+            value = attr_dict.get(key)
+            if value:
+                new_dict[key] = value
+        return new_dict
+    
+    def reload_from_json(self, json):
+
+        for key in json:
+            if hasattr(self ,key):
+                setattr(self, key, json.get(key))
