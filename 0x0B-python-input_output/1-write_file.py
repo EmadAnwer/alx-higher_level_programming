dcaa08e8aae46c7a365cb80aeca5a28497359e23
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 12:44:07 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""write file module"""
+
+def write_file(filename="", text=""):
+    """write_file
+    args:
+        filename: string
+        text: string
+    """
+    with open(filename, encoding="utf-8", mode= "w") as file:
+        return file.write(text)
