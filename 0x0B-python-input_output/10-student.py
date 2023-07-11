383e19259051838b5643730b21359ba84329fa20
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 20:59:24 +0200

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init
+        args:
+            first_name : string
+            last_name : string
+            age : int
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """class to json"""
+        attr_dict = self.__dict__
+        if attrs is None:
+            return attr_dict
+
+        new_dict = {}
+        for key in attrs:
+            value = attr_dict.get(key)
+            if value:
+                new_dict[key] = value
+        return new_dict
