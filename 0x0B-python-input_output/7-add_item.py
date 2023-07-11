b747934a63e3d56b2ef36b7c90a4e61bcfa1c361
Emad Anwer <emadanwer.official@gmail.com>
2023-07-11 17:05:54 +0200

    task 7 fix comment

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,11 @@
 
 from sys import argv
 
-#load functions
+# load functions
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 FILENAME = "add_item.json"
+
 try:
     json_obj = load_from_json_file(FILENAME)
     json_obj += argv[1:]
