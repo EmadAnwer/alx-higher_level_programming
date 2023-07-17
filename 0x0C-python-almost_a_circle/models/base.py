b79f60ebd2803d421aa3b9e743bfbc1a98c1b0be
Emad Anwer <emadanwer.official@gmail.com>
2023-07-17 20:24:18 +0200

    task 21

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,8 @@
 from json import dump
 from json import dumps
 from json import loads
-from json import load
+import csv
+
 
 class Base:
     """Base Class"""
@@ -61,7 +62,7 @@ class Base:
 
         instance.update(**dictionary)
         return instance
-    
+
     @classmethod
     def load_from_file(cls):
         """lode json from file"""
@@ -71,3 +72,39 @@ class Base:
         except FileNotFoundError:
             return []
         return list(map(lambda obj: cls.create(**obj), content))
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """save to file csv"""
+        mycsv = ""
+        flag = -1
+        for o in list_objs:
+            if o.__class__.__name__ == "Rectangle":
+                if flag == -1:
+                    mycsv += "id,width,height,x,y\n"
+                    flag = 0
+
+                mycsv += f"{o.id},{o.width},{o.height},{o.x},{o.y}\n"
+
+            else:
+                if flag == -1:
+                    mycsv += "id,size,x,y\n"
+                    flag = 0
+
+                mycsv += f"{o.id},{o.size},{o.x},{o.y}\n"
+
+        with open(cls.__name__+".csv", encoding="utf-8", mode="w") as file:
+            file.write(mycsv[:-1])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from file csv"""
+        data = []
+        with open(cls.__name__+".csv", encoding="utf-8", mode="r") as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                # Convert values to integers
+                for key in row:
+                    row[key] = int(row[key])
+                data.append(row)
+        return list(map(lambda obj: cls.create(**obj), data))
