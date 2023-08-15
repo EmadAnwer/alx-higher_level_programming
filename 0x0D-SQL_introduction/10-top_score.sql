d2e45a98687663438ae46719450ad7ad3e72d6f4
Emad Anwer <emadanwer.official@gmail.com>
2023-08-15 22:27:56 +0300

    task 10 fix output

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table second_table ORDER BY score DESC
 
-SELECT name, score FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC
