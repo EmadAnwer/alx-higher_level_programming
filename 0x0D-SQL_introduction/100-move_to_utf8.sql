852bc8a28f054ab7c8c2c84a108e9dff6f74f370
Emad Anwer <emadanwer.official@gmail.com>
2023-08-15 23:44:40 +0300

    task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,11 @@
---  converts hbtn_0c_0 database to UTF8
-ALTER DATABASE
-    hbtn_0c_0 
-    CHARACTER SET utf8mb4 
-    COLLATE utf8mb4_unicode_ci;
-
+-- Change the database to be used
 USE hbtn_0c_0;
-ALTER TABLE
-    first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
 
+-- Convert the table to utf8mb4
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE
-    first_table
-    MODIFY name 
-    VARCHAR(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+-- Convert the field 'name' to utf8mb4
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
