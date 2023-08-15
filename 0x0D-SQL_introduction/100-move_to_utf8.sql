97dc611dc794105ffc139bc4cee1f438b5e201b1
Emad Anwer <emadanwer.official@gmail.com>
2023-08-15 23:40:46 +0300

    fix task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,8 @@
---  score average of all records as "average"
-
+--  converts hbtn_0c_0 database to UTF8
 ALTER DATABASE
-    hbtn_0c_0
-    CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    hbtn_0c_0 
+    CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci;
 
 USE hbtn_0c_0;
 ALTER TABLE
