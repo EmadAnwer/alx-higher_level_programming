47c460fa069277712456c6d4280ca8c072a94ed8
Emad Anwer <emadanwer.official@gmail.com>
2023-08-17 00:07:33 +0300

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- create database and TABLE unique_id
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE 
+	IF NOT EXISTS hbtn_0d_usa.states
+	(
+		id 	INT
+			PRIMARY KEY
+			NOT NULL
+			AUTO_INCREMENT,
+		name VARCHAR(256)
+			 NOT NULL
+	);
