c8f793dd78ad4b829062b5d99fdd18782aa2ce44
Emad Anwer <emadanwer.official@gmail.com>
2023-08-17 18:10:09 +0300

    adv 1

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,9 @@
--- not Dexter
-
-SELECT tv_genres.name
-FROM tv_genres
-LEFT JOIN (SELECT tv_show_genres.genre_id AS id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter') AS Dexter_id
-ON Dexter_id.id = tv_genres.id
-WHERE Dexter_id.id IS NULL
+SELECT tv_genres.name FROM tv_genres 
+WHERE id NOT IN (
+	SELECT tv_show_genres.genre_id
+	FROM tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_show_genres.show_id =  tv_shows.id
+	WHERE tv_shows.title = 'Dexter'
+)
 ORDER BY tv_genres.name
