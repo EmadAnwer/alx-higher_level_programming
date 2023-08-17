3b4503f7403053486dfe6c4070b178b9ae182a5c
Emad Anwer <emadanwer.official@gmail.com>
2023-08-17 15:59:05 +0300

    fix

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,7 @@ LEFT JOIN (SELECT tv_show_genres.genre_id AS id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter') as Dexter_id
+WHERE tv_shows.title = 'Dexter') AS Dexter_id
 ON Dexter_id.id = tv_genres.id
 WHERE Dexter_id.id IS NULL
 ORDER BY tv_genres.name
