03188936c9283ce9c1c98b5986d52a472fd76522
Emad Anwer <emadanwer.official@gmail.com>
2023-09-12 00:26:29 +0300

    tesk 11 fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
-const secondBiggest = process.argv.slice(2).sort().reverse()[1];
-console.log(secondBiggest == undefined ? 0 : secondBiggest);
+const secondBiggest = process.argv
+  .slice(2)
+  .map(Number)
+  .sort((a, b) => b - a)[1];
+console.log(secondBiggest === undefined ? 0 : secondBiggest);
