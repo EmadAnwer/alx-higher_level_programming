94cc81577b3d242672a547128d8bfce8d680cdb9
Emad Anwer <emadanwer.official@gmail.com>
2023-09-11 22:05:07 +0300

    task 2 fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 console.log(
-  process.argv[2] === undefined ? 'No argument' : process.argv[3] === undefined ? 'Arguments found' : 'Argument found'
+  process.argv[2] === undefined ? 'No argument' : process.argv[3] === undefined ? 'Argument found' : 'Arguments found'
 );
