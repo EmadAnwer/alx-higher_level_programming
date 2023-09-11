a16bc8cc5a51bf9061f4dc6d0d2ddb11ac143a14
Emad Anwer <emadanwer.official@gmail.com>
2023-09-11 23:32:27 +0300

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let num = Number(process.argv[2]);
+if (num >= 0 || isNaN(num)) console.log(fact(num));
+
+function fact(number) {
+  if (number == 0 || isNaN(number)) return 1;
+  else return number * fact(number - 1);
+}
