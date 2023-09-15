521444f4cecb24d6d2cf7befb47af95bb44e6c9b
Emad Anwer <emadanwer.official@gmail.com>
2023-09-16 01:05:30 +0300

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Task 3 safe from MySQL injections!"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db_name
+    )
+    cursor = db.cursor()
+    all_states = cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC", (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
