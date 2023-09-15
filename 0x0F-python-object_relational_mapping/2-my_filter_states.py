09e460b29e3071ae55a9dd4853f83a62fa0677b9
Emad Anwer <emadanwer.official@gmail.com>
2023-09-15 21:43:16 +0300

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Task 2 takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument."""
+
+
 import MySQLdb
 from sys import argv
 
