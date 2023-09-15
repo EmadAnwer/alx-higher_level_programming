77fce5903d42eed780c72cb3e6f651264aad1e22
Emad Anwer <emadanwer.official@gmail.com>
2023-09-15 20:39:52 +0300

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Task 1 lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db_name
+    )
+    cursor = db.cursor()
+    query = """
+    SELECT * FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY id ASC
+    """
+    all_states = cursor.execute(query)
+
+    for i in cursor.fetchall():
+        print(i)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
