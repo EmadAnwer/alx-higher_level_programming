507eab0788f120f9205e10c9d206e3caa266fd8a
Emad Anwer <emadanwer.official@gmail.com>
2023-09-16 02:06:07 +0300

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Task 0 Get all states from  states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=mysql_db_name
+    )
+    cursor = db.cursor()
+    query = """
+    SELECT name FROM cities
+    WHERE cities.state_id = (SELECT id FROM states WHERE name = %s)
+    ORDER BY cities.id ASC"""
+    all_states = cursor.execute(query, (argv[4],))
+    flag = -1
+
+    for row in cursor.fetchall():
+        print(("" if flag == -1 else ", ") + row[0], end="")
+        flag = 1
+    print("")
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
