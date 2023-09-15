98f729fb337a8e0f3cf7a433369fb7a42e0bcaf9
Emad Anwer <emadanwer.official@gmail.com>
2023-09-15 20:54:42 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,14 +6,16 @@ from sys import argv
 
 if __name__ == "__main__":
     mysql_username, mysql_password, mysql_db_name = argv[1], argv[2], argv[3]
-
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=mysql_db_name
-    )
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=mysql_db_name
+        )
+    except Exception as e :
+        pass
     cursor = db.cursor()
     query = """
     SELECT * FROM states
