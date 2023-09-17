0e6efac5064eddf8303ef5f456493b6ef9e9692d
Emad Anwer <emadanwer.official@gmail.com>
2023-09-17 13:21:39 +0300

    t

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """create state model
 """
-
-
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,8 +9,6 @@ Base = declarative_base()
 
 
 class State(Base):
-    """create state model
-    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement='auto',
                 nullable=False, unique=True)
