8b2ce4ddab05bd87cd90b968b727c599be497874
Emad Anwer <emadanwer.official@gmail.com>
2023-09-28 19:25:26 +0300

    test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-if [ $(curl -Gs -o  /dev/null -w "%{http_code}" "${1}") -eq 200 ]; then curl -s -G "${1}"; fi
+curl -sL "$1"
