176b14117b95a56899d63962e61c78f66272b572
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 01:58:57 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Find peak in a list of unsorted integers
+'''Find peak in a list of unsorted integers'''''
 
 
 def find_peak(list_of_integers):
-    """Find peak in a list of unsorted integers"""""
+    '''Find peak in a list of unsorted integers'''
     list_of_integers = sorted(list_of_integers)
     return list_of_integers[-1]
