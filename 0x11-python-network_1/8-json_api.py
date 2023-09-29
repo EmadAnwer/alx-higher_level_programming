0abe8c1b6533661acdb409cc6003ec472d1d054b
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 19:22:06 +0300

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    value = ""
+    if len(sys.argv) > 1:
+        value = sys.argv[1]
+    data = {"q": value}
+    response = requests.post(sys.argv[1], data=data)
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get("id"),
+                                   json_response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
