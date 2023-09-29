9e2eb335afa5a44c190ee996dfe6801281a99250
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 16:44:53 +0300

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    data = urllib.parse.urlencode({"email":sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
