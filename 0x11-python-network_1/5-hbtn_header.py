09f43235829cbf3fc2e308ab1090a27621516703
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 18:18:59 +0300

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""  script that fetches https://alx-intranet.hbtn.io/status using requests"""
+"""displays the value of the variable X-Request-Id"""
 
 if __name__ == "__main__":
     import requests
     import sys
     req = requests.get(sys.argv[1])
-    print(req.headers['X-Request-Id'])
+    print(req.headers["X-Request-Id"])
