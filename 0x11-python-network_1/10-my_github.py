bf70c2591335efe0870de18c757617350a50c82b
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 20:27:25 +0300

    task 10 fix pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     import requests
     import sys
     headers = {'Authorization': f'Bearer {sys.argv[2]}',
-               'X-GitHub-Api-Version': '2022-11-28', 
+               'X-GitHub-Api-Version': '2022-11-28',
                'Accept': 'application/vnd.github.v3+json'}
-    url ='https://api.github.com/user'
+    url = 'https://api.github.com/user'
     req = requests.get(url, headers=headers)
     print(req.json().get('id'))
