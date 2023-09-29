d604837f810d326fc593793bbb301955c81d8e74
Emad Anwer <emadanwer.official@gmail.com>
2023-09-29 16:46:13 +0300

    task 2 fix pycodestyle - space :(

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import urllib.request
     import urllib.parse
     import sys
-    data = urllib.parse.urlencode({"email":sys.argv[2]})
+    data = urllib.parse.urlencode({"email": sys.argv[2]})
     data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
