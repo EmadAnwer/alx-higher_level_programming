4d17278ef67cc313e18a3127dabb1464eea58d23
Emad Anwer <emadanwer.official@gmail.com>
2023-09-30 17:25:08 +0300

    adv1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,11 @@
 if __name__ == "__main__":
     import requests
     import sys
-    req = requests.get(f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits')
-    for commit in req.json()[0:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+    req = requests.get(
+        f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits')
+    for commit in range(10):
+        try:
+            print(req.json()[commit].get('sha'), end=': ')
+            print(req.json()[commit].get('commit').get('author').get('name'))
+        except:
+            pass
