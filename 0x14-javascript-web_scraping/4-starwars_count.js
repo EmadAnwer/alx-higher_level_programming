0a3372d7a2b9de575dfae3a5eca5eb15c96b2f54
Emad Anwer <emadanwer.official@gmail.com>
2023-10-24 17:34:47 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,8 @@ const request = require('request');
 const url = process.argv[2];
 if (!url) process.exit();
 request.get(url, (error, response, body) => {
-  let count = 0;
   if (!error) {
-    try {
-      const films = JSON.parse(body);
-      for (let index = 0; index < films.count; index++) {
-        for (const characterUrl of films.results[index].characters) {
-          if (characterUrl.endsWith('/18/')) count++;
-        }
-      }
-      console.log(count);
-    } catch (error) {
-      console.log(count);
-    }
+    const count = body.split('/people/18/').length - 1;
+    console.log(count);
   }
 });
