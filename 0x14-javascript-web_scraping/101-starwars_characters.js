c46f9812f692e353f9d8940249c0ce3a0b44e54e
Emad Anwer <emadanwer.official@gmail.com>
2023-10-24 19:49:30 +0300

    adv 2

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// get request
+const request = require('request');
+const movieId = process.argv[2];
+if (!movieId) process.exit();
+
+const filmsURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const makeRequest = (url) =>
+  new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) reject(error);
+      else {
+        console.log(JSON.parse(body).name);
+        resolve();
+      }
+    });
+  });
+request.get(filmsURL, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const charactersInMovie = JSON.parse(body).characters;
+    for (const char of charactersInMovie) {
+      makeRequest(char);
+    }
+  }
+});
