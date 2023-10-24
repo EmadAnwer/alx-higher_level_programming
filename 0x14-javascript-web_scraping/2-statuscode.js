ef164eb77e4413ed012187fa968eadd4a14ed0d4
Emad Anwer <emadanwer.official@gmail.com>
2023-10-24 15:23:16 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request.get(process.argv[2], (error, response, body) => {
-  console.log('code:', response.statusCode);
+  if (!error) console.log('code:', response.statusCode);
 });
