06f778a33ae744e20f03f9849b5bb6301fa95914
Emad Anwer <emadanwer.official@gmail.com>
2023-10-24 15:03:45 +0300

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//read arg file
+const fs = require('fs');
+fs.readFile('' + process.argv[2], 'utf8', (error, data) => {
+  if (!error) {
+    console.log(data);
+  } else console.log(error);
+});
