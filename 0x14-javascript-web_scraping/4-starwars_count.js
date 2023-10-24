b503c2a313886f221ba69cf334e43a8e05ccdb8c
Emad Anwer <emadanwer.official@gmail.com>
2023-10-24 17:08:16 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,22 @@ const request = require('request');
 const url = process.argv[2];
 if (!url) process.exit();
 request.get(url, (error, response, body) => {
-  const characterID = '/18/';
+  const characterID = '18';
   let count = 0;
   if (!error && response.statusCode === 200) {
-    try {
-      const films = JSON.parse(body);
-      for (let index = 0; index < films.count; index++) {
-        for (const character of films.results[index].characters) {
-          if (character.search(characterID) !== -1) count++;
-        }
+    const data = JSON.parse(body);
+    const movies = data.results;
+    let count = 0;
+
+    for (const movie of movies) {
+      if (
+        movie.characters.includes(
+          `https://swapi-api.alx-tools.com/api/people/${characterID}/`
+        )
+      ) {
+        count++;
       }
-      console.log(count);
-    } catch (error) {
-      console.log(error);
     }
+    console.log(count);
   }
 });
